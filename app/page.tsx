14d8e43ee9b884a22d618edd3d2745f159b429cd
ibrahimshoshaaa <brahimsosa9@gmail.com>
2026-09-19 15:06:15 +0300

    fix: restore client homepage loading flow

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
 import HomeClient from "./components/HomeClient"
-import { fetchProducts, fetchSettings } from "./lib/api"
 
-export default async function HomePage() {
-  const [products, settings] = await Promise.all([
-    fetchProducts(),
-    fetchSettings(),
-  ])
-
-  return <HomeClient initialProducts={products} initialSettings={settings} />
+export default function HomePage() {
+  return <HomeClient />
 }
